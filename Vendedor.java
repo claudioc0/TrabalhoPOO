c76c440105de265e800ac6bc6368858af5944a14
claudioc0 <165798717+claudioc0@users.noreply.github.com>
2024-05-15 16:36:41 -0300

    Add files via upload

--- Vendedor.java
+++ Vendedor.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Vendedor extends Pessoa{
@@ -6,17 +8,33 @@ public class Vendedor extends Pessoa{
 
     //ATRIBUTOS
     private int avaliacoes;
+    private List<Jogo> jogosAnunciados;
 
-    //CONTRUTOR
-    public Vendedor(String nome, String email, String senha, int cpf){
+    // Construtor
+    public Vendedor(String nome, String email, String senha, int cpf) {
         super(nome, email, senha, cpf);
+        jogosAnunciados = new ArrayList<>();
+    }
+
+    // Método para anunciar jogo
+    public void anunciarJogo(Jogo jogo) {
+        jogosAnunciados.add(jogo);
+        System.out.println("Jogo anunciado com sucesso!");
+    }
+
+    // Método para exibir jogos anunciados
+    public void exibirJogosAnunciados() {
+        System.out.println("Jogos anunciados:");
+        for (Jogo jogo : jogosAnunciados) {
+            System.out.println(jogo.getNomejogo());
+        }
     }
 
     public void anuciarJogo(){
         System.out.println("anuncia jogo");
     }
 
-
+    // Método para cadastrar Vendedor
     public void cadastrarVendedor(){
         Vendedor vendedor = new Vendedor(getNome(), getEmail(), getSenha(), getCpf());
 
